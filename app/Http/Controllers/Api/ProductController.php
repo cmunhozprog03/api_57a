f7cc0aacaf661dc6037bb402ce4d720561c03baf
cmunhozprog <cmunhozprog03@hotmail.com>
2020-09-29 13:19:05 -0300

    Aula 16 Completa

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Product;
+class ProductController extends Controller
+{
+    /**
+     * @var Product
+     */
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function index()
+    {
+        $products = $this->product->paginate(1);
+
+        return response()->json($products);
+    }
+
+    public function show($id)
+    {
+        $products = $this->product->find($id);
+        return response()->json($products);
+    }
+
+    public function save(Request $request)
+    {
+        $data = $request->all();
+        $product = $this->product->create($data);
+        return response()->json($product);
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+
+        $product = $this->product->find($data['id']);
+        $product->update($data);
+
+        return response()->json($product);
+
+    }
+
+    public function delete($id)
+    {
+        $product = $this->product->find($id);
+        $product->delete();
+
+        return response()->json(['data' => ['msg' => 'Produto removido com sucesso']]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,28 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function(Request $request){
+
+    //dd($request->headers->all());
+    //dd($request->headers->get('Authorization'));
+
+    $response = new \Illuminate\Http\Response(json_encode(['msg' => 'Minha Primeira resposta de Api' ]));
+    $response->header('Content-Type', 'application/json');
+    return $response;
+});
+// ROUTE PRODUCT
+Route::namespace('Api')->group(function (){
+    Route::prefix('products', function(){
+        Route::get('/', 'ProductController@index');
+        Route::get('/{id}', 'ProductController@show');
+        Route::post('/', 'ProductController@save');
+        Route::put('/', 'ProductController@update');
+        Route::patch('/', 'ProductController@update');
+        Route::delete('/{id}', 'ProductController@delete');
+    });
+
+    Route::resource('/users', 'UserController');
+
+});
+
